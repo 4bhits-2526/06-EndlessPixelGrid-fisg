c77302ed070800cf45aad168796120d7a31d1fc5
gfischinger <georg.fischinger07@htl-salzburg.ac.at>
2026-01-30 13:52:41 +0100

    update PixelGridModel

--- Assets/MyGame/Scripts/PixelGridModel.cs
+++ Assets/MyGame/Scripts/PixelGridModel.cs
@@ -1,22 +1,7 @@
-public class PixelGridModel {
-    public bool[,] grid;
-    public bool[] inputLine;
+using UnityEngine;
 
-    public PixelGridModel() {
-        grid = new bool[10, 7];
-        inputLine = new bool[7];
-    }
-
-    public void ClearInputLine() {
-        for (int i = 0; i < inputLine.Length; i++)
-            inputLine[i] = false;
-    }
-
-    public void ClearGrid() {
-        for (int r = 0; r < grid.GetLength(0); r++)
-            for (int c = 0; c < grid.GetLength(1); c++)
-                grid[r, c] = false;
-
-        ClearInputLine();
-    }
+public class PixelModel : MonoBehaviour {
+    // true = weiß, false = schwarz
+    public bool[] inputLine = new bool[7];
+    public bool[,] grid = new bool[10, 7];
 }
