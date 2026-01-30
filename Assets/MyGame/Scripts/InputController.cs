1e0814c5abe85ce430424d8fc9287d23d7decf32
gfischinger <georg.fischinger07@htl-salzburg.ac.at>
2026-01-30 14:39:24 +0100

    Render grid and input line from data model only

--- Assets/MyGame/Scripts/InputController.cs
+++ Assets/MyGame/Scripts/InputController.cs
@@ -1,4 +1,4 @@
-using UnityEngine;
+﻿using UnityEngine;
 using UnityEngine.UI;
 
 public class InputController : MonoBehaviour {
@@ -13,10 +13,31 @@ public class InputController : MonoBehaviour {
         if (Input.GetKeyDown(KeyCode.RightArrow)) TogglePixel(4);
         if (Input.GetKeyDown(KeyCode.DownArrow)) TogglePixel(5);
         if (Input.GetKeyDown(KeyCode.S)) TogglePixel(6);
+
+        if (Input.GetKeyDown(KeyCode.D)) {
+            PushInputLineIntoGrid();
+        }
     }
 
     void TogglePixel(int index) {
         model.inputLine[index] = !model.inputLine[index];
         inputImages[index].color = model.inputLine[index] ? Color.white : Color.black;
+    }
+
+    void PushInputLineIntoGrid() {
+        for (int row = 0; row < model.grid.GetLength(0) - 1; row++) {
+            for (int col = 0; col < model.grid.GetLength(1); col++) {
+                model.grid[row, col] = model.grid[row + 1, col];
+            }
+        }
+
+        int lastRow = model.grid.GetLength(0) - 1;
+        for (int col = 0; col < model.grid.GetLength(1); col++) {
+            model.grid[lastRow, col] = model.inputLine[col];
+        }
+
+        for (int i = 0; i < model.inputLine.Length; i++) {
+            model.inputLine[i] = false;
+        }
     }
 }
--- Assets/MyGame/Scripts/PixelModel.cs
+++ Assets/MyGame/Scripts/PixelModel.cs
@@ -1,7 +1,7 @@
 using UnityEngine;
 
 public class PixelModel : MonoBehaviour {
-    // true = weiﬂ, false = schwarz
     public bool[] inputLine = new bool[7];
     public bool[,] grid = new bool[10, 7];
+
 }
